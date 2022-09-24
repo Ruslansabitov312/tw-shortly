45a7ce3df3ecbbf8b640e15c1d7184f014a78e0a
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2022-09-25 00:12:58 +0600

    Js part - link validation

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const input = document.getElementById('link-input')
+const linkForm = document.getElementById('link-form')
+const errMsg = document.getElementById('err-msg')
+
+linkForm.addEventListener('submit', formSubmit)
+
+// Validate a URL
+function validURL(str) {
+  var pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      '((\\d{1,3}\\.){3}\\d{1,3}))' +
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+      '(\\?[;&a-z\\d%_.~+=-]*)?' +
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  )
+  return !!pattern.test(str)
+}
+
+function formSubmit(e) {
+  e.preventDefault()
+
+  if (input.value === '') {
+    errMsg.innerHTML = 'Please enter something.'
+    input.classList.add('border-red')
+  } else if (!validURL(input.value)) {
+    errMsg.innerHTML = 'Please enter a valid URL.'
+    input.classList.add('border-red')
+  } else {
+    errMsg.innerHTML = ''
+    input.classList.remove('border-red')
+    alert('Success')
+  }
+}
