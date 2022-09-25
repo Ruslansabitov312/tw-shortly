0f4c68944b0fe898b141f73096964ca6bea9f98f
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2022-09-26 01:13:38 +0600

    Mobile menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+
 const input = document.getElementById('link-input')
 const linkForm = document.getElementById('link-form')
 const errMsg = document.getElementById('err-msg')
 
+btn.addEventListener('click', navToggle)
 linkForm.addEventListener('submit', formSubmit)
+
+// Toggle Mobile Menu
+function navToggle() {
+  btn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
+}
 
 // Validate a URL
 function validURL(str) {
